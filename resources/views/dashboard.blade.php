76d7f72f97b82c61fecaa46409a895512df54887
haidi20 <nhaidii@yahoo.com>
2019-03-25 12:02:30 +0800

    add-background-for-content

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,20 +1,18 @@
 @extends('_layouts.default')
 
 @section('content')
-<div class="content-body">
-	<div class="row">
-		<div class="col-md-12">
-            <div class="card-header">
-                <h3 class="card-title">Dashboard</h3>
-                <a class="heading-elements-toggle"><i class="icon-ellipsis font-medium-3"></i></a>
-                <div class="heading-elements">
-                    <ul class="list-inline mb-0">
-                        {{-- <li><a data-action="collapse"><i class="icon-minus4"></i></a></li> --}}
-                        {{-- <li><a data-action="expand"><i class="icon-expand2"></i></a></li> --}}
-                    </ul>
+<div class="page-body">
+    <div class="row">
+        <div class="col-sm-12">
+             <div class="card">
+                <div class="card-header">
+                    <h2>Dashboard</h2>
+                </div>
+                <div class="card-block">
+                    <p>content</p>
                 </div>
             </div>
-		</div>
-	</div>
-</div>	
+        </div>
+    </div>
+</div>
 @endsection
