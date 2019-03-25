f8a3033bb39bbf31d9b12db44a7e60cf5c4b05b7
haidi20 <nhaidii@yahoo.com>
2019-03-25 11:47:28 +0800

    update-header-for-logout

--- resources/views/_layouts/basic.blade.php
+++ resources/views/_layouts/basic.blade.php
@@ -28,44 +28,7 @@
     
 </head>
 <body>
-  <!-- Pre-loader start -->
-  <div class="theme-loader">
-      <div class="ball-scale">
-          <div class='contain'>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-              <div class="ring">
-                  <div class="frame"></div>
-              </div>
-          </div>
-      </div>
-  </div>
-  <!-- Pre-loader end -->
+  @include('_layouts.loading')
 
   @yield('basic-content')
 
--- resources/views/_layouts/header.blade.php
+++ resources/views/_layouts/header.blade.php
@@ -80,6 +80,67 @@
                     </a>
                 </li>
             </ul>
+            <ul class="nav-right">
+                        <li class="header-notification">
+                            <div class="dropdown-primary dropdown">
+                                <div class="dropdown-toggle" data-toggle="dropdown">
+                                    <i class="feather icon-bell"></i>
+                                    <span class="badge bg-c-pink">5</span>
+                                </div>
+                                <ul class="show-notification notification-view dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                                    <li>
+                                        <h6>Notifications</h6>
+                                        <label class="label label-danger">New</label>
+                                    </li>
+                                    <li>
+                                        <div class="media">
+                                            <img class="d-flex align-self-center img-radius" src="https://vignette.wikia.nocookie.net/naruto/images/7/7b/Kurama2.png/revision/latest?cb=20140818171718" alt="Generic placeholder image">
+                                            <div class="media-body">
+                                                <h5 class="notification-user">John Doe</h5>
+                                                <p class="notification-msg">Lorem ipsum dolor sit amet, consectetuer elit.</p>
+                                                <span class="notification-time">30 minutes ago</span>
+                                            </div>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="header-notification">
+                            <div class="dropdown-primary dropdown">
+                                <div class="displayChatbox dropdown-toggle" data-toggle="dropdown">
+                                    <i class="feather icon-message-square"></i>
+                                    <span class="badge bg-c-green">3</span>
+                                </div>
+                            </div>
+                        </li>
+                        <li class="user-profile header-notification">
+                            <div class="dropdown-primary dropdown">
+                                <div class="dropdown-toggle" data-toggle="dropdown">
+                                    {{-- <img src="../files/assets/images/avatar-4.jpg" class="img-radius" alt="User-Profile-Image"> --}}
+                                    <span>Username</span>
+                                    <i class="feather icon-chevron-down"></i>
+                                </div>
+                                <ul class="show-notification profile-notification dropdown-menu" data-dropdown-in="fadeIn" data-dropdown-out="fadeOut">
+                                    <li>
+                                        <a href="#!">
+                                            <i class="feather icon-settings"></i> Settings
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="user-profile.html">
+                                            <i class="feather icon-user"></i> Profile
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="auth-normal-sign-in.html">
+                                            <i class="feather icon-log-out"></i> Logout
+                                        </a>
+                                    </li>
+                                </ul>
+
+                            </div>
+                        </li>
+                    </ul>
         </div>
     </div>
 </nav>
