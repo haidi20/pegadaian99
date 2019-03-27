e1e87d9678aafcbe02e2177e45b27a81b1c3948d
haidi20 <nhaidii@yahoo.com>
2019-03-27 12:21:11 +0800

    update-form-akad-baru-data-rahin

--- resources/views/akad/form.blade.php
+++ resources/views/akad/form.blade.php
@@ -69,7 +69,7 @@
                     <div class="form-group row">
                         {{-- <label class="col-sm-2 col-form-label" for="investor">Investor</label> --}}
                         <div class="col-sm-10">
-                            <input type="date" class="form-control" name="tanggal_akad" id="tanggal_akad">
+                            <input type="date" class="form-control" name="tanggal_akad" id="tanggal_akad" value="{{old('tanggal_akad')}}">
                         </div>
                     </div>
                 </div>
@@ -192,6 +192,96 @@
                             <input type="text" class="form-control" name="terbilang" id="terbilang" value="{{old('terbilang')}}" disabled>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-sm-12">
+             <div class="card">
+                <div class="card-block">
+                    <h3 class="sub-title">Data Rahin</h3>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="nama_lengkap">Nama Lengkap</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="nama_lengkap" id="nama_lengkap" value="{{old('nama_lengkap')}}" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" >Jenis Kelamin</label>
+                        <div class="col-sm-10">
+                            <div class="form-radio">
+                                <div class="radio radio-inline">
+                                    <label>
+                                        <input type="radio" name="jenis_kelamin" value="pria" checked="{{checked('pria', 'jenis_kelamin')}}">
+                                        <i class="helper"></i>Pria
+                                    </label>
+                                </div>
+                                <div class="radio radio-inline">
+                                    <label>
+                                        <input type="radio" name="jenis_kelamin" value="wanita" checked="{{checked('wanita', 'jenis_kelamin')}}">
+                                        <i class="helper"></i>Wanita
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" >Alamat</label>
+                        <div class="col-sm-10">
+                            <textarea rows="5" cols="5" class="form-control" id="alamat" name="alamat" >{{old('alamat')}}</textarea>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="kota">Kota</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="kota" id="kota" value="{{old('kota')}}" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="no_telp">No. Telp</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="no_telp" id="no_telp" value="{{old('no_telp')}}" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" >Jenis Identitas</label>
+                        <div class="col-sm-10">
+                            <div class="form-radio">
+                                <div class="radio radio-inline">
+                                    <label>
+                                        <input type="radio" name="jenis_identitas" value="KTP" checked="{{checked('KTP', 'jenis_identitas')}}">
+                                        <i class="helper"></i>KTP
+                                    </label>
+                                </div>
+                                <div class="radio radio-inline">
+                                    <label>
+                                        <input type="radio" name="jenis_identitas" value="SIM" checked="{{checked('SIM', 'jenis_identitas')}}">
+                                        <i class="helper"></i>SIM
+                                    </label>
+                                </div>
+                                <div class="radio radio-inline">
+                                    <label>
+                                        <input type="radio" name="jenis_identitas" value="KK" checked="{{checked('KK', 'jenis_identitas')}}">
+                                        <i class="helper"></i>KK
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="no_identitas">No. Identitas</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="no_identitas" id="no_identitas" value="{{old('no_identitas')}}" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <label class="col-sm-2 col-form-label" for="tanggal_lahir">Tanggal Lahir</label>
+                        <div class="col-sm-10">
+                            <input type="date" class="form-control" name="tanggal_lahir" id="tanggal_lahir" value="{{old('tanggal_lahir')}}">
+                        </div>
+                    </div>
+                    <button type="submit" class="btn btn-primary btn-xs">Proses</button>
                 </div>
             </div>
         </div>
