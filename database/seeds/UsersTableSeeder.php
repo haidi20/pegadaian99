99e8d38e85224942bc8e35c4631f8520f752336a
haidi20 <nhaidii@yahoo.com>
2019-03-27 21:37:10 +0800

    update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -12,20 +12,10 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        // User::insert([
-        // 	'username' 	=> 'admin',
-        // 	'password'	=> bcrypt('admin'),
-        // 	'level'		=> 'admin',
-        // 	'created_at'=> Carbon::now(),
-        // 	'updated_at'=> Carbon::now(),
-        // ]);
-
         $user = new User;
         $user->username = 'admin';
         $user->password = bcrypt('admin123');
         $user->level    = 'admin';
-        $user->created_at= Carbon::now();
-        $user->updated_at= Carbon::now();
         $user->save();
     }
 }
