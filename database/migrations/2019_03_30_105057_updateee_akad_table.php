d5633a2cd8e93d3865f6e11e23585880e7e4ed6c
haidi20 <nhaidii@yahoo.com>
2019-03-31 01:24:37 +0800

    update-table-akad-column-jangka_waktu_akad

---
+++ database/migrations/2019_03_30_105057_updateee_akad_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateeeAkadTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+       DB::statement("ALTER TABLE
+    akad
+        MODIFY COLUMN
+            jangka_waktu_akad enum(
+                '60',
+                '30',
+                '7',
+                '1'
+            )
+        NOT NULL AFTER kekurangan;") ;       
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+         Schema::table('akad', function($table){
+            // $table->dropColumn('jangka_waktu', ['7', '1']);
+        });
+    }
+}
