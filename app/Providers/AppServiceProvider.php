6bf2c170217b09622fe61c93d8f16bca02d00ef5
haidi20 <nhaidii@yahoo.com>
2019-04-25 10:56:49 +0800

    update-permodalan-form-penambahan_saldo-

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Laravel\Dusk\DuskServiceProvider;// Importing DuskServiceProvider class
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -24,5 +25,9 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
          require_once app_path('Supports/Helpers.php');
+
+          if ($this->app->environment('local', 'testing')) {
+            $this->app->register(DuskServiceProvider::class);
+        }
     }
 }
