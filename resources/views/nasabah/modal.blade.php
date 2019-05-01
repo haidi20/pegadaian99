fd67e64d8c5e7325aed473838cc2a0f81556ea7b
haidi20 <nhaidii@yahoo.com>
2019-05-02 07:55:06 +0800

    update-database-data_nasabah-modal

--- resources/views/nasabah/modal.blade.php
+++ resources/views/nasabah/modal.blade.php
@@ -8,54 +8,7 @@
                 </button>
             </div>
             <div class="modal-body">
-                <div class="view-info">
-                    <div class="row">
-                        <div class="col-lg-12 col-sm-12">
-                            <div class="general-info">
-                                <div class="row" id="data-loading">
-                                    <div class="col-lg-12 col-xl-12 col-sm-12">
-                                        <div align="center">
-                                            <div class="loader-block" id="loader-block">
-                                                <div class="preloader3 loader-block">
-                                                    <div class="circ1 loader-primary"></div>
-                                                    <div class="circ2 loader-primary"></div>
-                                                    <div class="circ3 loader-primary"></div>
-                                                    <div class="circ4 loader-primary"></div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="row" id="data-detail">
-                                    <div class="col-sm-12 col-md-7">
-                                        <div class="table-responsive">
-                                            <table class="table m-0">
-                                                <tbody id="table-detail-one">
-                                                    {{-- fetch data from jquery --}}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                    <div class="col-sm-12 col-md-5">
-                                        <div class="table-responsive">
-                                            <table class="table m-0" >
-                                                <tbody id="table-detail-two">
-                                                   {{-- fetch data from jquery --}}
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                    <!-- end of table col-lg-6 -->
-                                </div>
-                                <!-- end of row -->
-                            </div>
-                            <!-- end of general info -->
-                        </div>
-                        <!-- end of col-lg-12 -->
-                    </div>
-                    <!-- end of row -->
-                </div>
-                <!-- end of view-info -->
+                <h1>coba</h1>
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-default btn-success waves-effect " data-dismiss="modal">Oke</button>
@@ -97,3 +50,4 @@
         </div>
     </div>
 </div>
+
