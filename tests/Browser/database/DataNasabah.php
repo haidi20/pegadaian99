862247473c83f48c98d94cb1aa00faaaf0c0f118
yogithesymbian <yogirenbox33@gmail.com>
2019-05-09 09:26:10 -0400

    dusk-filtering-data

--- tests/Browser/database/DataNasabah.php
+++ tests/Browser/database/DataNasabah.php
@@ -9,15 +9,66 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 class DataNasabah extends DuskTestCase
 {
     /**
+     * declare
+     * #path_url-02
+     * url.com/cabang
+     * path direct after login
+     */
+    public function cabang_path()
+    {
+        return '/cabang';
+    }
+    /**
      * A Dusk test example.
-     *
+     * @test
      * @return void
      */
-    public function testExample()
+    public function user_view_data_nasabah()
     {
+
+        /**
+         * first todo
+         * todo auth|login|middleware
+         * alternative use ->loginAs(Model::find(id))
+         */
+        $this->user_login();
         $this->browse(function (Browser $browser) {
-            $browser->visit('/')
-                    ->assertSee('Laravel');
+            $browser
+                /**
+                 * #path_url-02 cabang_path()
+                 * don't forget to put path link after execute link/move/change address.url
+                 * in this case we have move from user_login
+                 */
+                ->assertPathIs($this->cabang_path())
+                ->assertSee('Data Cabang')
+                // going to Database
+                ->clickLink('Database')
+                /**
+                 * ?clickLink('param')
+                 * the function for this
+                 * <a href='x'> param </a>
+                 * move to CabangCreate | Tambah Cabang
+                 */
+                ->clickLink('Data Nasabah')
+                ->assertSee('DATA TABLE NASABAH')
+                ->screenshot('UserViewDataCabang[1]view')
+                // user want show 50 data per page
+                ->select('perpage', '50')
+                ->screenshot('UserViewDataCabang[2]viewPerPage')
+                // user search ( filter : ['No Telp', '082250256655']) + FIELD VALUE
+                ->select('by', 'no_telp')
+                // some case value more prefered use ID not CLASS , sometime make anError
+                ->value('#q', '082250256655')
+                ->screenshot('UserViewDataCabang[3]FilterView')
+                ->press('Oke')
+                ->assertSee('DEVI YULISTIA ANGGRENI')
+                ->screenshot('UserViewDataCabang[4]FilterSubmit')
+                // ->click('#detail_957')
+                // ->clickLink('Detail Data')
+                // ->assertSee('Detail Data Nasabah')
+                // ->screenshot('UserViewDataCabang[5]DetailViewNasabah')
+                // end
+            ;
         });
     }
 }
