c9cb51d4dc1fc002f1a0b2f41702cf52cb0f50b1
yogithesymbian <yogirenbox33@gmail.com>
2019-05-09 09:52:10 -0400

    update-dusk-view

--- tests/Browser/database/DataAkad.php
+++ tests/Browser/database/DataAkad.php
@@ -5,9 +5,9 @@ namespace Tests\Browser\database;
 use Tests\DuskTestCase;
 use Laravel\Dusk\Browser;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
-use Tests\Browser\Login;
+use Tests\Browser\database\DataNasabah;
 
-class DataAkad extends Login
+class DataAkad extends DataNasabah
 {
 
     /**
@@ -22,15 +22,28 @@ class DataAkad extends Login
          * todo auth|login|middleware
          * alternative use ->loginAs(Model::find(id))
          */
-        // $this->user_login();
+        $this->user_login();
         $this->browse(function (Browser $browser) {
             $browser
                 /**
-             * #path_url-02 cabang_path()
-             * don't forget to put path link after execute link/move/change address.url
-             * in this case we have move from user_login
-             */
-
+                 * #path_url-02 cabang_path()
+                 * don't forget to put path link after execute link/move/change address.url
+                 * in this case we have move from user_login
+                 */
+                ->assertPathIs($this->cabang_path())
+                ->assertSee('Data Cabang')
+                // going to Database
+                ->clickLink('Database')
+                /**
+                 * ?clickLink('param')
+                 * the function for this
+                 * <a href='x'> param </a>
+                 * move to CabangCreate | Tambah Cabang
+                 */
+                ->clickLink('Data Akad Nasabah')
+                ->assertSee('Data Akad')
+                ->assertSee('DEFAULT')
+                ->screenshot('UserViewDataAkadNasabah[1]')
                 // end
             ;
         });
