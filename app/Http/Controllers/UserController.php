fffd45fc5de406dac4dd25169b923c2e2b1caecf
haidi20 <nhaidii@yahoo.com>
2019-05-10 11:25:04 +0800

    update

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -30,8 +30,6 @@ class UserController extends Controller
 
     public function index()
     {
-       return $this->getLatLong(request()->ip());
-
     	$user = $this->user->get();
 
     	// proccess get value 'nomor cabang'
@@ -109,27 +107,5 @@ class UserController extends Controller
     	$user->delete();
 
     	return redirect()->back();
-    }
-
-    // function to get  the address
-    public function getLatLong($address){
-        if(!empty($address)){
-            //Formatted address
-            $formattedAddr = str_replace(' ','+',$address);
-            //Send request and receive json data by address
-            $geocodeFromAddr = file_get_contents('http://maps.googleapis.com/maps/api/geocode/json?address='.$formattedAddr.'&sensor=false'); 
-            $output = json_decode($geocodeFromAddr);
-            //Get latitude and longitute from json data
-            $data['latitude']  = $output->results[0]->geometry->location->lat; 
-            $data['longitude'] = $output->results[0]->geometry->location->lng;
-            //Return latitude and longitude of the given address
-            if(!empty($data)){
-                return $data;
-            }else{
-                return false;
-            }
-        }else{
-            return false;   
-        }
     }
 }
