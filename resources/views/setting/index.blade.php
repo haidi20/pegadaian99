7095ee356f4a5cd600a57e394d165ea1b455e383
haidi20 <nhaidii@yahoo.com>
2019-05-13 09:30:29 +0800

    update-setting-table

--- resources/views/setting/index.blade.php
+++ resources/views/setting/index.blade.php
@@ -65,7 +65,7 @@
                 },
                 ajaxOptions: {
                     dataType: 'JSON',
-                    type: 'POST'
+                    type: 'GET'
                 },
                 // display: function(value, response) {
                 //     console.log(response.coba);
