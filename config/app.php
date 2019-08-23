63aaeb3ae4614f530cd5cdea8af291f46aca5626
haidi20 <nhaidii@yahoo.com>
2019-08-23 11:12:58 +0800

    update-database-data_pembayaran-pendapatan-masih_bermasalah_total

--- config/app.php
+++ config/app.php
@@ -181,7 +181,7 @@ return [
         // \Torann\GeoIP\GeoIPServiceProvider::class,
 
         Riskihajar\Terbilang\TerbilangServiceProvider::class,
-        // Sayeed\CustomMigrate\CustomMigrateServiceProvider::class,
+        Sayeed\CustomMigrate\CustomMigrateServiceProvider::class,
 
     ],
 
